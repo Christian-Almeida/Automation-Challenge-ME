d1ad3eee81f48667d22fde22dbed2bc73bd5006c
Christian Almeida <chris_adn@hotmail.com>
2021-02-23 11:33:02 -0300

    Corrigindo Env e Deixando o Firefox como driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,9 +7,12 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'report_builder'
 
+
 Capybara.configure do |config|
-  config.default_max_wait_time = 10             #deixando o capybara aguardar no minimo 5s para aparecer um elemento 
-  config.app_host ='http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation'
-  Capybara.ignore_hidden_elements = false
-  Capybara.page.driver.browser.manage.window.maximize  # Maximizando a tela
+  
+    config.default_driver = :selenium
+    config.default_max_wait_time = 10             #deixando o capybara aguardar no minimo 5s para aparecer um elemento 
+    config.app_host ='http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation'
+    Capybara.ignore_hidden_elements = false
+    Capybara.page.driver.browser.manage.window.maximize    # Maximizando a tela
 end
